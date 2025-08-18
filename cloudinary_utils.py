207540257a166b681899663a14043a66313a072b
Alexandr <kalmykovalexander28@gmail.com>
2025-08-18 17:11:26 +0200

    changed

--- cloudinary_utils.py
+++ cloudinary_utils.py
@@ -3,10 +3,16 @@ import os
 import cloudinary.uploader
 
 # Cloudinary configuration
+# cloudinary.config(
+#     cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+#     api_key=os.getenv("CLOUDINARY_API_KEY"),
+#     api_secret=os.getenv("CLOUDINARY_API_SECRET")
+# )
+
 cloudinary.config(
-    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
-    api_key=os.getenv("CLOUDINARY_API_KEY"),
-    api_secret=os.getenv("CLOUDINARY_API_SECRET")
+    cloud_name="djzcaugul",
+    api_key="973924154917292",
+    api_secret="tiYTBhu78EXnkQLXqUi-0sR_uUc"
 )
 
 def upload_image(temp_path):
